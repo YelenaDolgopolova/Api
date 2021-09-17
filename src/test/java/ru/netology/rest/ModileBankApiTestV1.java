74968786ae03d5eb70968d319b422f73dcb7d29c
YelenaDolgopolova <dolgopolova-lena.95@yandex.ru>
2021-09-17 22:18:22 +0500

    change file

--- src/test/java/ru/netology/rest/ModileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/ModileBankApiTestV1.java
@@ -8,14 +8,11 @@ import static io.restassured.RestAssured.given;
 public class ModileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
+
         given()
                 .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
-                // Проверки
                 .then()
                 .statusCode(200);
     }
